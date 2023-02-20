94ef85c9986f5c3318e240f8a2783af0b4fba1d1
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-20 23:20:44 +0800

    add login & signup

--- frontend/kanji/src/app/components/login/login.component.ts
+++ frontend/kanji/src/app/components/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
--- frontend/kanji/src/app/components/signup/signup.component.ts
+++ frontend/kanji/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
