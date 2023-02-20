63809d724989be747b70d18ff191e6dd078c433e
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-20 20:14:03 +0800

    update kanji-detail comp to display some basic info

--- frontend/kanji/src/app/components/kanji/kanji-detail/kanji-detail.component.ts
+++ frontend/kanji/src/app/components/kanji/kanji-detail/kanji-detail.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { KanjiService } from 'src/app/service/kanji.service';
+import { KanjiInfo } from 'src/app/model/KanjiInfo';
 
 @Component({
   selector: 'app-kanji-detail',
   templateUrl: './kanji-detail.component.html',
   styleUrls: ['./kanji-detail.component.css']
 })
-export class KanjiDetailComponent {
+export class KanjiDetailComponent implements OnInit {
+  kanji: any = {} as any;
+  loaded: boolean = false;
+  kanjiInfo: KanjiInfo[] = [];
+  cols: string[] = [
+    "accent",
+    "example"
+  ]
+
+  constructor(
+    private kanjiService: KanjiService,
+    private route: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit(): void {
+      this.getKanji();
+  }
+
+  getKanji() {
+    this.kanjiService.getKanjiList().subscribe(kanjiList => {
+      for (let kanji of kanjiList) {
+        for (let alter of kanji.kanji) {
+          if (alter === this.route.snapshot.paramMap.get('kanji')) {
+            this.kanji = kanji;
+            this.kanjiInfo = this.generateKanjiInfomation();
+            console.log(this.kanjiInfo)
+            this.loaded = true;
+          }
+        }
+      }
+    })
+  } 
+
+  generateKanjiInfomation(): KanjiInfo[] {
+    let res: KanjiInfo[] = [];
+    for (let i = 0; i < this.kanji.accent.length; ++i) {
+      res.push({
+        'accent': this.kanji.accent[i],
+        'example': this.kanji.example[i]
+      })
+    }
+    return res;
+  }
+
+  getKanjiMainChar(): string {
+    return this.kanji.kanji[0];
+  }
+
+  getKanjiAlternative(): string {
+    return this.kanji.kanji.slice(1, this.kanji.kanji.length).join(', ');
+  }
 
 }
