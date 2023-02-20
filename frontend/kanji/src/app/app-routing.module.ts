c8f61bec74d814ce8cb0f185e3b040a44f1d51b6
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-21 00:09:13 +0800

    update routing & dependency

--- frontend/kanji/src/app/app-routing.module.ts
+++ frontend/kanji/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { KanjiListComponent } from './components/kanji/kanji-list/kanji-list.com
 import { KanjiDetailComponent } from './components/kanji/kanji-detail/kanji-detail.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { NormalComponent } from './components/cards/normal/normal.component';
 
 const routes: Routes = [
   { path: '', component: KanjiDetailComponent },
   { path: 'register', component: SignupComponent }, 
   { path: 'login', component: LoginComponent },
   { path: 'kanji', component: KanjiListComponent },
-  { path: 'kanji/:kanji', component: KanjiDetailComponent }
+  { path: 'kanji/:kanji', component: KanjiDetailComponent },
+  { path: 'card/normal', component: NormalComponent}
 ];
 
 @NgModule({
--- frontend/kanji/src/app/app.module.ts
+++ frontend/kanji/src/app/app.module.ts
@@ -26,6 +26,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NormalComponent } from './components/cards/normal/normal.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -38,7 +40,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     KanjiDetailComponent,
     LoginComponent,
     SignupComponent,
-    ProfileComponent
+    ProfileComponent,
+    NormalComponent,
   ],
   imports: [
     BrowserModule,
@@ -57,7 +60,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
