295be3ed7bcb137da778f66f76ac1b2b90c14234
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-21 20:14:12 +0800

    update login comp

--- frontend/kanji/src/app/components/login/login.component.ts
+++ frontend/kanji/src/app/components/login/login.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/service/login.service';
+import { UtilsService } from 'src/app/service/utils/utils.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+export class LoginComponent implements OnInit {
+  username: string = "";
+  constructor(
+    private loginService: LoginService,
+    private utils: UtilsService,
+    private cookie: CookieService,
+    private router: Router
+  ) {
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  ngOnInit(): void {
+      
+  }
+
+  login(): void {
+    this.loginService.login(this.username).subscribe(resp => {
+      this.cookie.set('user', resp.userId)
+      this.loginService.setLoggedInUser(resp);
+      this.router.navigate(['/']);
+    }, (e: HttpErrorResponse) => {
+      this.utils.openSnackBar(e.error.message, 'OK');
+    })
   }
 }
