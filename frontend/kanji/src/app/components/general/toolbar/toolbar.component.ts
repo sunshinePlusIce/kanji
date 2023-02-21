eecc175969658fc7044c619922f10f515021d207
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-21 20:18:57 +0800

    remove unnecessary injections

--- frontend/kanji/src/app/components/general/toolbar/toolbar.component.ts
+++ frontend/kanji/src/app/components/general/toolbar/toolbar.component.ts
@@ -1,7 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AvatarService } from 'src/app/service/avatar.service';
 import { LoginService } from 'src/app/service/login.service';
 import { SidenavService } from 'src/app/service/sidenav.service';
 
@@ -14,20 +13,8 @@ export class ToolbarComponent {
   constructor(
     public sidenavService: SidenavService,
     private loginService: LoginService,
-    private avatarService: AvatarService,
-    private router: Router
   ) {
 
-  }
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.avatarService.getRandomAvatar('hello').subscribe(resp => {
-      console.log(resp)
-    }, (e: HttpErrorResponse) => {
-      console.log(e)
-    })
   }
 
   isLoggedIn(): boolean {
