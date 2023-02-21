376165cf45624a79deeca12612bff33f12b6cc54
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-21 20:15:58 +0800

    display random avatar on toolbar

--- frontend/kanji/src/app/components/general/toolbar/toolbar.component.ts
+++ frontend/kanji/src/app/components/general/toolbar/toolbar.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AvatarService } from 'src/app/service/avatar.service';
+import { LoginService } from 'src/app/service/login.service';
 import { SidenavService } from 'src/app/service/sidenav.service';
 
 @Component({
@@ -8,8 +12,33 @@ import { SidenavService } from 'src/app/service/sidenav.service';
 })
 export class ToolbarComponent {
   constructor(
-    public sidenavService: SidenavService
+    public sidenavService: SidenavService,
+    private loginService: LoginService,
+    private avatarService: AvatarService,
+    private router: Router
   ) {
 
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.avatarService.getRandomAvatar('hello').subscribe(resp => {
+      console.log(resp)
+    }, (e: HttpErrorResponse) => {
+      console.log(e)
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.loginService.isLoggedIn();
+  }
+
+  getLoggedInUser(): any {
+    return this.loginService.getLoggedInUser();
+  }
+
+  logout(): void {
+    this.loginService.logout();
   }
 }
