f02afd045195ed260ee9b1af53e3d7c3c520d3b3
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-21 20:16:57 +0800

    update routing

--- frontend/kanji/src/app/app-routing.module.ts
+++ frontend/kanji/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { KanjiListComponent } from './components/kanji/kanji-list/kanji-list.component';
 import { KanjiDetailComponent } from './components/kanji/kanji-detail/kanji-detail.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { NormalComponent } from './components/cards/normal/normal.component';
 
 const routes: Routes = [
   { path: '', component: KanjiDetailComponent },
-  { path: 'register', component: SignupComponent }, 
   { path: 'login', component: LoginComponent },
   { path: 'kanji', component: KanjiListComponent },
   { path: 'kanji/:kanji', component: KanjiDetailComponent },
