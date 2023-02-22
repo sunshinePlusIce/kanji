505dc68728be52047eaeb12ba59f9474eb018485
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-22 19:27:27 +0800

    add book comp

---
+++ frontend/kanji/src/app/components/glossary/book/book.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+  @Input() book: any = {} as any;
+}
