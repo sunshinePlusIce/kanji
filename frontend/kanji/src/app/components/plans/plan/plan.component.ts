84654b5717fec3e30521cadc1bf4ca8412beff5f
sunshinePlusIce <sunshineplusice@gmail.com>
2023-02-22 19:32:35 +0800

    add plan comp

---
+++ frontend/kanji/src/app/components/plans/plan/plan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan',
+  templateUrl: './plan.component.html',
+  styleUrls: ['./plan.component.css']
+})
+export class PlanComponent {
+
+}
